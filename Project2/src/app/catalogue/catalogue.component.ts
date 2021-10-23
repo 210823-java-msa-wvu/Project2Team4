47d882eb24400133bb14f822feeaf09a0e012d64
rcamp07 <33884089+rcamp07@users.noreply.github.com>
2021-10-22 19:21:43 -0500

    implemented basic routing logic on front end

--- Project2/src/app/catalogue/catalogue.component.ts
+++ Project2/src/app/catalogue/catalogue.component.ts
@@ -11,7 +11,9 @@ import { SongService } from '../services/song.service';
 export class CatalogueComponent implements OnInit {
 
   /*for 2 way binding */
-  name: string = " ";
+  musician_name: string = " ";
+
+  song_name: string = " ";
 
   songs: Song[] = [];
 
