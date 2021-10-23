287697b5d7dc0c71b9fe7f7e3ced1b67367085de
rcamp07 <33884089+rcamp07@users.noreply.github.com>
2021-10-23 17:21:24 -0500

    almost functioning login

--- Project2/src/main/java/com/revature/controllers/UserController.java
+++ Project2/src/main/java/com/revature/controllers/UserController.java
@@ -20,19 +20,27 @@ public class UserController {
         this.userRepo = userRepo;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping
     public List<User> getAllUsers() {
         return userRepo.findAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(path="/{id}")
     public User getById(@PathVariable("id") int id) {return userRepo.getById(id);}
 
+  @CrossOrigin(origins = "http://localhost:4200")
+  @GetMapping(path="/{username}")
+  public User getByUsername(@PathVariable("username") String username) {return userRepo.getByUsername(username);}
+
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping(consumes = "application/json", produces = "application/json")
     public User addUser(@RequestBody User user) {
         return userRepo.save(user);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PutMapping(path="/{id}")
     public void updateUser(@PathVariable("id") int id, @RequestBody User user) {
         if ( id == user.getId()) {
@@ -40,6 +48,7 @@ public class UserController {
         }
     }
 
+   @CrossOrigin(origins = "http://localhost:4200")
    @DeleteMapping(path="/{id}")
    public void deleteUser(@PathVariable("id") int id) {
             userRepo.delete(userRepo.getById(id));
