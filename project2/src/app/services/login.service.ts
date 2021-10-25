3bf41ffe51771338268a010ee9e9e6a265345c43
rcamp07 <33884089+rcamp07@users.noreply.github.com>
2021-10-25 15:50:37 -0500

    Fuctioning login, started on registration

--- project2/src/app/services/login.service.ts
+++ project2/src/app/services/login.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user'; 
 
+//deprecated in favor user.service
+
 @Injectable({
   providedIn: 'root'
 })
